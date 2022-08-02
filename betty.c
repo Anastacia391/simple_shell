a02088813578027a1cf9275eed410c8e8f835aa0
hawkinswinja <hawkinswinja@gmail.com>
2022-08-02 10:39:28 +0300

    successful betty code

---
+++ betty.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - program to print
+  * Description: prints hell world correctly using betty lint
+  * Return: always 0 on success
+  */
+int main(void)
+{
+	printf("Hello World\n");
+	return (0);
+}
